955df546a44fcca115b7679ce72ae03d871e6043
GloryDream413 <putnikdeveloperbg@gmail.com>
2023-05-07 16:43:37 +0900

    Setting Dialog NetType function changed.

--- client/src/components/SettingDlg/index.js
+++ client/src/components/SettingDlg/index.js
@@ -95,9 +95,9 @@ function SettingDlg({
                                 checked={netType === "testnet" ? false : true}
                                 onClick={() => {
                                     if (netType === "testnet")
-                                        setNetType("testnet");
-                                    else
                                         setNetType("mainnet");
+                                    else
+                                        setNetType("testnet");
                                 }} />
                             <div className="w-14 h-7 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                             <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">{netType}</span>
